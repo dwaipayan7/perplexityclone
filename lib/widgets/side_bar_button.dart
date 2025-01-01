0babbfd5fb6a7f422c561f7180da2c7c16370829
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-01-01 18:26:14 +0530

    updated

--- lib/widgets/side_bar_button.dart
+++ lib/widgets/side_bar_button.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 import '../theme/colors.dart';
 
+
+
 class SideBarButton extends StatelessWidget {
   final bool isCollapsed;
   final IconData icon;
   final String text;
-
   const SideBarButton({
     super.key,
     required this.isCollapsed,
@@ -16,32 +17,31 @@ class SideBarButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment:
-      isCollapsed ? MainAxisAlignment.center : MainAxisAlignment.start,
-      children: [
-        Container(
-          margin: const EdgeInsets.symmetric(vertical: 14, horizontal: 10),
-          child: Icon(
-            icon,
-            color: AppColors.iconGrey,
-            size: 22,
-          ),
-        ),
-        if (!isCollapsed)
-          Flexible(
-            child: Text(
-              text,
-              style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w500,
-                color: Colors.white,
-              ),
-              overflow: TextOverflow.ellipsis,
-              maxLines: 1,
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        mainAxisAlignment:
+        isCollapsed ? MainAxisAlignment.center : MainAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 14, horizontal: 10),
+            child: Icon(
+              icon,
+              color: AppColors.iconGrey,
+              size: 22,
             ),
           ),
-      ],
+          isCollapsed
+              ? const SizedBox()
+              : Text(
+            text,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
