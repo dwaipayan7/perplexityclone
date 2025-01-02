36840e19dff0257fbe861c34a20aa18b9a92f41c
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-01-02 20:41:50 +0530

    Enhance search functionality by integrating sorting and LLM services; update configuration for API keys

--- server/config.py
+++ server/config.py
@@ -5,4 +5,5 @@ from pydantic_settings import BaseSettings
 load_dotenv()
 
 class Settings(BaseSettings):
-    TAVILY_API_KEY: str
+    TAVILY_API_KEY: str = ""
+    GEMINI_API_KEY: str = ""
--- server/main.py
+++ server/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-
 from pydantic_models.chat_body import ChatBody
 from services.search_service import SearchService
+from services.sort_source_service import SortSourceService
 
 #Testing FastAPI
 # print("Dwaipayan")
@@ -11,13 +11,18 @@ from services.search_service import SearchService
 
 app = FastAPI()
 search_service = SearchService()
+sort_source_service = SortSourceService()
 
 
 @app.post("/chat")
 def chat_endpoint(body: ChatBody):
     #search web find sources
-    search_service.web_search(body.query)
+    search_results = search_service.web_search(body.query)
+    # print(search_results)
     #sort sources
+    sorted_results = sort_source_service.sort_sources(body.query, search_results)
+
+    print(sorted_results)
     #generate the response
     
     return body.query
